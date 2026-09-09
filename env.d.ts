f1eebc3e701f933ccbf50a6ff3e9d7983cfb8c0d
Arash Altafi <arashaltafi1377@gmail.com>
2026-09-09 09:25:39 +0330

    add configs

---
+++ env.d.ts
@@ -0,0 +1,11 @@
+namespace NodeJS {
+  interface ProcessEnv {
+    DB_USER: string;
+    DB_PASSWORD: string;
+    DB_SERVER: string;
+    DB_PORT: string;
+    DB_DATABASE : string;
+    DB_ENCRYPT : string;
+    DB_TRUST_SERVER_CERTIFICATE : string;
+  }
+}
